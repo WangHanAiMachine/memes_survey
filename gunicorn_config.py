f82676932df43e470f5325154578120feaccbad9
WangHanAiMachine <han_wang@sutd.edu.sg>
2023-09-05 16:11:49 +0800

    debug

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,10 @@
 bind = "0.0.0.0:80"
 workers = 2
 # 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  WangHanAiMachine
+# echo "# memes_survey" >> README.md
+# git init
+# git add .
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin git@github.com:WangHanAiMachine/memes_survey.git
+# git push -u origin main
